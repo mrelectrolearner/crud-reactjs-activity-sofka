e58dfc98ad81b47c6815812c765b46e1afac58ef
luis <luisfelorivas@gmai.com>
2022-05-02 14:51:23 -0500

    add the funcion to uncompleted a todo.

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -40,7 +40,7 @@ function TodoProvider(props) {
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
+    newTodos[todoIndex].completed =!todos[todoIndex].completed;
     saveTodos(newTodos);
   };
 
